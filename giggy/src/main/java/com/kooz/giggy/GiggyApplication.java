a2cbfe6b4d6c94a6e857d49d2f3408563ceb7bbc
Jeongsu Yeo <duwjdtn1@gmail.com>
2024-08-30 21:41:16 +0900

    ADD: Springboot project

---
+++ giggy/src/main/java/com/kooz/giggy/GiggyApplication.java
@@ -0,0 +1,13 @@
+package com.kooz.giggy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiggyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiggyApplication.class, args);
+	}
+
+}
---
+++ giggy/src/test/java/com/kooz/giggy/GiggyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kooz.giggy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiggyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
