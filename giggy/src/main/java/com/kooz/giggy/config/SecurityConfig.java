e094e34f037ad042f8e3fabfafd66e0f64738c94
Jeongsu Yeo <duwjdtn1@gmail.com>
2024-09-18 16:42:21 +0900

    FEAT: config 내 로그인, JWT 필터 적용

--- giggy/src/main/java/com/kooz/giggy/config/SecurityConfig.java
+++ giggy/src/main/java/com/kooz/giggy/config/SecurityConfig.java
@@ -1,18 +1,29 @@
 package com.kooz.giggy.config;
 
+import com.kooz.giggy.util.JWTFilter;
+import com.kooz.giggy.util.JwtUtil;
+import com.kooz.giggy.util.LoginFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final AuthenticationConfiguration configuration;
+    private final JwtUtil jwtUtil;
+
+    private final String[] allowsUrls = {"/", "/swagger-ui/**", "/auth", "/sign-in", "/sign-up"};
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -23,10 +34,16 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )   // 세션 없이 jwt 기반으로 진행
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/").permitAll()
-                        .requestMatchers("/auth/**").permitAll()    //auth는 누구나 가능하도록
+                        .requestMatchers(allowsUrls).permitAll() // allowsUrls의 경우 permit
                         .anyRequest().authenticated() // 그 외엔 모두 인증 필요
-                );
+                )
+                .sessionManagement(sessionManagement -> sessionManagement
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                // 기존 UsernamePWAuthentcationFilter를 로그인 필터로 대체
+                .addFilterAt(new LoginFilter(configuration.getAuthenticationManager(), jwtUtil), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
+
 //                .oauth2Login(oauth2 -> oauth2
 //                                .successHandler(oauth2SuccessHandler)
 //                                .userInfoEndpoint(Customizer.withDefaults())
@@ -41,5 +58,10 @@ public class SecurityConfig {
 
 
         return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
